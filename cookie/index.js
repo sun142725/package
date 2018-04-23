012b9d6012cbf6ced0e241f36696925ba760745c
sun142725 <421818732@qq.com>
2018-04-23 13:25:06 +0800

    cookie

---
+++ cookie/index.js
@@ -0,0 +1,23 @@
+module.exports = {
+    setCookie: function (name,value,day) {
+        var Days = day;
+        var exp = new Date();
+        exp.setTime(exp.getTime() + Days*24*60*60*1000);
+        document.cookie = name + "="+ escape (value) + ";expires=" + exp.toGMTString();
+    },
+    getCookie: function (name) {
+        var arr,reg=new RegExp("(^| )"+name+"=([^;]*)(;|$)");
+        if(arr=document.cookie.match(reg))
+            return unescape(arr[2]);
+        else
+            return null;
+    },
+    delCookie: function (name) {
+        cookie.setCookie(name, "", -1)
+        // var exp = new Date();
+        // exp.setTime(exp.getTime() - 1);
+        // var cval=getCookie(name);
+        // if(cval!=null)
+        //     document.cookie= name + "="+cval+";expires="+exp.toGMTString();
+    }
+}
